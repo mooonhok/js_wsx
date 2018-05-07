331926cb266880441c5d7a13f7e2ebee9df1ff4f
mooonhok <1026413232@qq.com>
2018-05-07 14:29:21 +0800

    修改

--- index.php
+++ index.php
@@ -30,6 +30,6 @@ $app->get('/', function ($request, $response, $args) {
 $app->get('/hello[/{name}]', function ($request, $response, $args) {
     $response->write("Hello, " . $args['name']);
     return $response;
-});
+})->setArgument('name', 'World!');
 
 $app->run();
