930b79300c0d08834f4b04e309d0eb7356f912f7
GHOSTDO <2121066061@qq.com>
2018-05-15 15:06:48 +0800

    xiugai
    Signed-off-by: GHOSTDO <2121066061@qq.com>

--- user.php
+++ user.php
@@ -72,6 +72,115 @@ $app->post('/addUser',function(Request $request,Response $response){
     }
 });
 
+$app->get('/getUser0',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $database=localhost();
+    $phone=$request->getParam('phone');
+    if($phone!=null||$phone!=""){
+        $selectStatement = $database->select()
+            ->from('user')
+            ->where('phone','=',$phone);
+        $stmt = $selectStatement->execute();
+        $data = $stmt->fetch();
+        if($data!=null){
+             return $response->withJson(array("result" => "0", "desc" => "success",'user'=>$data));
+        }else{
+            return $response->withJson(array("result"=>"2","desc"=>"用户不存在"));
+        }
+    }else{
+        return $response->withJson(array("result"=>"1","desc"=>"缺少电话"));
+    }
+});
+
+$app->get('/getUser1',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $database=localhost();
+    $phone=$request->getParam('phone');
+    $passwd=$request->getParam('passwd');
+    if($phone!=null||$phone!=""){
+        if($passwd!=null||$passwd!=""){
+        $selectStatement = $database->select()
+            ->from('user')
+            ->where('passwd','=',encode($passwd , 'cxphp'))
+            ->where('phone','=',$phone);
+        $stmt = $selectStatement->execute();
+        $data = $stmt->fetch();
+        if($data!=null){
+            return $response->withJson(array("result" => "0", "desc" => "success",'user'=>$data));
+        }else{
+            return $response->withJson(array("result"=>"2","desc"=>"用户不存在"));
+        }
+        }else{
+            return $response->withJson(array("result"=>"3","desc"=>"缺少密码"));
+        }
+    }else{
+        return $response->withJson(array("result"=>"1","desc"=>"缺少电话"));
+    }
+});
+
+$app->get('/getUser2',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $database=localhost();
+    $name=$request->getParam('name');
+    $number=$request->getParam('number');
+    if($name!=null||$name!=""){
+        if($number!=null||$number!=""){
+            $selectStatement = $database->select()
+                ->from('user')
+                ->where('number','=',$number)
+                ->where('name','=',$name);
+            $stmt = $selectStatement->execute();
+            $data = $stmt->fetch();
+            if($data!=null){
+                return $response->withJson(array("result" => "0", "desc" => "success",'user'=>$data));
+            }else{
+                return $response->withJson(array("result"=>"2","desc"=>"用户不存在"));
+            }
+        }else{
+            return $response->withJson(array("result"=>"3","desc"=>"缺少身份号"));
+        }
+    }else{
+        return $response->withJson(array("result"=>"1","desc"=>"缺少姓名"));
+    }
+});
+
+
+$app->put('/alterUser0',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $database=localhost();
+    $body = $request->getBody();
+    $body=json_decode($body);
+    $id=$body->id;
+    $passwd=$body->passwd;
+    if($id!=null||$id!=""){
+        if($passwd!=null||$passwd!=""){
+            $selectStatement = $database->select()
+                ->from('user')
+                ->where('id','=',$id);
+            $stmt = $selectStatement->execute();
+            $data = $stmt->fetch();
+            if($data!=null){
+                $array['passwd']=encode($passwd , 'cxphp');
+                $updateStatement = $database->update($array)
+                    ->table('user')
+                    ->where('id','=',$id);
+                $affectedRows = $updateStatement->execute();
+                return $response->withJson(array("result" => "0", "desc" => "success"));
+            }else{
+                return $response->withJson(array("result"=>"2","desc"=>"用户不存在"));
+            }
+        }else{
+            return $response->withJson(array("result"=>"3","desc"=>"缺少密码"));
+        }
+    }else{
+        return $response->withJson(array("result"=>"1","desc"=>"缺少id"));
+    }
+});
+
 
 
 //$checkProxyHeaders = true;
