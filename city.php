8c260feaa97bd89389bf1792bdfb7365b70fc338
mooonhok <1026413232@qq.com>
2018-05-15 15:45:25 +0800

    修改

--- city.php
+++ city.php
@@ -75,9 +75,49 @@ $app->post('/getvaluedemo',function(Request $request,Response $response){
 });
 
 
-$checkProxyHeaders = true;
-$trustedProxies = ['10.0.0.1', '10.0.0.2'];
-$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
+//$checkProxyHeaders = true;
+//$trustedProxies = ['10.0.0.1', '10.0.0.2'];
+//$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
+
+$app->options('/alterUser0',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $response=$response->withAddedHeader("Access-Control-Allow-Methods", "PUT");
+    return $response;
+});
+$app->put('/alterUser0',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $database=localhost();
+    $body = $request->getBody();
+    $body=json_decode($body);
+    $id=$body->id;
+    $passwd=$body->passwd;
+    $array=array();
+    if($id!=null||$id!=""){
+        if($passwd!=null||$passwd!=""){
+            $selectStatement = $database->select()
+                ->from('user')
+                ->where('id','=',$id);
+            $stmt = $selectStatement->execute();
+            $data = $stmt->fetch();
+            if($data!=null){
+                $array['passwd']=encode($passwd , 'cxphp');
+                $updateStatement = $database->update($array)
+                    ->table('user')
+                    ->where('id','=',$id);
+                $affectedRows = $updateStatement->execute();
+                return $response->withJson(array("result" => "0", "desc" => "success"));
+            }else{
+                return $response->withJson(array("result"=>"2","desc"=>"用户不存在"));
+            }
+        }else{
+            return $response->withJson(array("result"=>"3","desc"=>"缺少密码"));
+        }
+    }else{
+        return $response->withJson(array("result"=>"1","desc"=>"缺少id"));
+    }
+});
 
 $app->run();
 
