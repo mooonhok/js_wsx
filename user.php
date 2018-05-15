67c5293bd956685675fb85060208086f99f3b8e5
mooonhok <1026413232@qq.com>
2018-05-15 12:35:01 +0800

    修改

--- user.php
+++ user.php
@@ -78,9 +78,9 @@ $app->post('/addUser',function(Request $request,Response $response){
 
 
 
-$checkProxyHeaders = true;
-$trustedProxies = ['10.0.0.1', '10.0.0.2'];
-$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
+//$checkProxyHeaders = true;
+//$trustedProxies = ['10.0.0.1', '10.0.0.2'];
+//$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
 
 $app->run();
 
