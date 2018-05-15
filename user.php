29580a66ce2ffdaa2e5c43641e265612baf1ce1d
GHOSTDO <2121066061@qq.com>
2018-05-15 09:56:29 +0800

    xiugai
    Signed-off-by: GHOSTDO <2121066061@qq.com>

---
+++ user.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/15
+ * Time: 9:45
+ */
+require 'vendor/autoload.php';
+require 'connect.php';
+
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\PDO\Database;
+use Slim\PDO\Statement;
+use Slim\PDO\Statement\SelectStatement;
+$app = new Slim\App();
+
+$app->post('/addUser',function(Request $request,Response $response){
+    $database=localhost();
+    $body = $request->getBody();
+    $body=json_decode($body);
+    $phone=$body->phone;//获取body中数据
+    $name=$body->name;
+    $number=$body->number;
+    $passwd=$body->passwd;
+    $array=array();
+    foreach($body as $key=>$value){
+        $array[$key]=$value;
+    }
+    if($phone!=null||$phone!=""){
+        if($name!=null||$name!=""){
+            if($number!=null||$number!=""){
+                if($passwd!=null||$passwd!=""){
+                    $selectStatement = $database->select()
+                        ->from('user');
+                    $stmt = $selectStatement->execute();
+                    $data = $stmt->fetchAll();
+                    $array['id']=count($data);
+                    $insertStatement = $database->insert(array_keys($array))
+                        ->into('user')
+                        ->values(array_values($array));
+                    $insertId = $insertStatement->execute(false);
+                    echo json_encode(array("result" => "0", "desc" => "success"));
+                }else{
+                    echo  json_encode(array("result"=>"4","desc"=>"缺少密码"));
+                }
+            }else{
+                echo  json_encode(array("result"=>"3","desc"=>"缺少身份证号"));
+            }
+        }else{
+            echo  json_encode(array("result"=>"2","desc"=>"缺少姓名"));
+        }
+    }else{
+        echo  json_encode(array("result"=>"1","desc"=>"缺少电话"));
+    }
+});
+
+
+
+
+
+$checkProxyHeaders = true;
+$trustedProxies = ['10.0.0.1', '10.0.0.2'];
+$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
+
+$app->run();
+
+function localhost()
+{
+    return connect();
+}
+
+?>
