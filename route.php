ab37620916ff5cd81e93dd66f5af65a226e59f71
GHOSTDO <2121066061@qq.com>
2018-05-16 14:01:51 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

---
+++ route.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/16
+ * Time: 13:57
+ */
+require 'vendor/autoload.php';
+require 'connect.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\PDO\Database;
+use Slim\PDO\Statement;
+use Slim\PDO\Statement\SelectStatement;
+
+$app = new \Slim\App();
+
+$app->get('/getRoutes',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $database=localhost();
+    $selectStatement = $database->select()
+        ->from('route');
+        $stmt = $selectStatement->execute();
+        $data = $stmt->fetchAll();
+        if($data!=null){
+            return $response->withJson(array("result" => "0", "desc" => "success",'user'=>$data));
+        }else{
+            return $response->withJson(array("result"=>"2","desc"=>"尚未有数据"));
+        }
+});
+
+
+
+
+$app->run();
+
+function localhost()
+{
+    return connect();
+}
+?>
