a32389cffea24f7340f0173374bfe43e88e6c151
GHOSTDO <2121066061@qq.com>
2018-06-15 13:51:14 +0800

    xg
    Signed-off-by: GHOSTDO <2121066061@qq.com>

---
+++ rate.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/16
+ * Time: 13:57
+ */
+require 'vendor/autoload.php';
+require 'connect.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\PDO\Database;
+use Slim\PDO\Statement;
+use Slim\PDO\Statement\SelectStatement;
+
+$app = new \Slim\App();
+
+$app->get('/getRoute1',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $database=localhost();
+    $transfer_type=$request->getParam('transfer_type');//获取请求路径后数据
+    $selectStatement = $database->select()
+        ->from('rate')
+        ->where('trabsfer_type','=',$transfer_type);
+    $stmt = $selectStatement->execute();
+    $data = $stmt->fetchAll();
+    if($data!=null){
+        for($i=0;$i<count($data);$i++){
+            $selectStatement = $database->select()
+                ->from('type')
+                ->where('id','=',$data[$i]['lorry_type']);
+            $stmt = $selectStatement->execute();
+            $data2= $stmt->fetch();
+            $data[$i]['lorry_type']=$data2;
+            $selectStatement = $database->select()
+                ->from('distance')
+                ->where('id','=',$data[$i]['distance']);
+            $stmt = $selectStatement->execute();
+            $data3= $stmt->fetch();
+            $data[$i]['diatance']=$data3;
+        }
+    }
+    return $response->withJson(array("result" => "0", "desc" => "success",'rates'=>$data));
+});
+
+
+$app->options('/alterRate',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $response=$response->withAddedHeader("Access-Control-Allow-Methods", "PUT");
+    return $response;
+});
+
+
+$app->put('/alterRate',function(Request $request,Response $response){
+    $response=$response->withAddedHeader('Access-Control-Allow-Origin','*');
+    $response=$response->withAddedHeader('Content-Type','application/json');
+    $database=localhost();
+    $body = $request->getBody();
+    $body=json_decode($body);
+    $id=$body->id;
+    $array=array();
+    foreach($body as $key=>$value){
+        if($key!="id"){
+            $array[$key]=$value;
+        }
+    }
+    if($id!=null||$id!=""){
+        $updateStatement = $database->update($array)
+            ->table('rate')
+            ->where('id','=',$id);
+        $affectedRows = $updateStatement->execute();
+        return $response->withJson(array("result" => "0", "desc" => "success"));
+    }else{
+        return $response->withJson(array("result"=>"1","desc"=>"id为空"));
+    }
+});
+
+
+
+
+$app->run();
+function array_unset_tt($arr,$key){
+    //建立一个目标数组
+    $res = array();
+    foreach ($arr as $value) {
+        //查看有没有重复项
+        if(isset($res[$value[$key]])){
+            //有：销毁
+            unset($value[$key]);
+        }
+        else{
+            $res[$value[$key]] = $value;
+        }
+    }
+    return $res;
+}
+
+function localhost()
+{
+    return connect();
+}
+?>
